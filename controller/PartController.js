450e2e1e2bcbd3dce89aaa115c787841a1c492ee
TwinMine <michaelrolo11@gmail.com>
2026-04-28 16:50:51 +0200

    add getData function

--- controller/PartController.js
+++ controller/PartController.js
@@ -1,31 +1,31 @@
-import Part from "../models/Part"
-
+import Part from "../models/Part";
 
 export const createPart = async (req, res, next) => {
-    try{
-const {
-  partNumber,
-  name,
-  quantity,
-  price,
-  lowLimit,
-  description
-} = req.body;
+  try {
+    const { partNumber, name, quantity, price, lowLimit, description } =
+      req.body;
 
-const newPart = new Part({
-  partNumber,
-  name,
-  quantity,
-  price,
-  lowLimit,
-  description,
-  userId: req.user.id,
+    const newPart = new Part({
+      partNumber,
+      name,
+      quantity,
+      price,
+      lowLimit,
+      description,
+      userId: req.user.id,
+    });
 
-});
+    res.status(200).json({ data: newPart, message: "Part created" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-
-res.status(200).json({data: newPart, message: "Part created"})
-    } catch(error){
-        next(error)
-    }
-}
+export const getData = async (req, res, next) => {
+  try {
+    const parts = await Part.find({ userId: req.user.id, deleted: false });
+    res.json({data: parts, message: "Data found!"});
+  } catch (error) {
+    next(error);
+  }
+};
