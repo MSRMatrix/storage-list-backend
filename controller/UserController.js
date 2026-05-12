82b082b60e26f5536ec677ba696e23009cf02af0
TwinMine <michaelrolo11@gmail.com>
2026-05-12 16:33:19 +0200

    add login function

--- controller/UserController.js
+++ controller/UserController.js
@@ -4,9 +4,6 @@ export const createUser = async (req, res, next) => {
   try {
     const { username, email, password, createdAt, company, currency, deleted } =
       req.body;
-   if (await User.findOne({ email: email })) {
-      return res.status(404).json({ message: "Email already exist!" });
-    }
 
     const newUser = new User({
       username: username || "Nicht verfügbar",
@@ -21,6 +18,39 @@ export const createUser = async (req, res, next) => {
     await newUser.save();
     
     res.status(200).json({ data: newUser, message: "User created" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const searchEmail = await User.findOne({ email });
+
+    if (!searchEmail) {
+      const message = "Email-Adresse wurde nicht gefunden!";
+      return res.status(404).json({ message });
+    }
+
+    const passwordCompare = await comparePassword(
+      password,
+      searchEmail.password
+    );
+
+    if (!passwordCompare) {
+      const message = "Passwort stimmt nicht!";
+      res.status(404).json({ message });
+    }
+
+    const token = issueJwt(searchEmail);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+    res.status(200).json({ searchEmail, token });
   } catch (error) {
     next(error);
   }
