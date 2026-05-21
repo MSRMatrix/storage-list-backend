3fbc9c27a42095aa0d00cfe08ee6a42d0895ece9
TwinMine <michaelrolo11@gmail.com>
2026-05-21 08:24:46 +0200

    add login, logout and getData to userRouter

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -1,11 +1,21 @@
 import express from "express"
-import { createUser } from "../controller/UserController";
+import { createUser, getData, login, logout } from "../controller/UserController";
 import { userValidator, validateRequest } from "../middlewares/validator/validatorFunctions";
 
 
 const router = express.Router()
 
 router
+.route("/").post(getData)
+
+router
 .route("/create").post(userValidator, validateRequest, createUser)
+
+router
+.route("/login").post(login)
+
+router
+.route("/logout").post(logout)
+
 
 export default router;
