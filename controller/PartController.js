c4b62ef9fb1f2e094cad2e2427257965f8d3a5ef
TwinMine <michaelrolo11@gmail.com>
2026-06-26 09:12:15 +0200

    add editPart to controller

--- controller/PartController.js
+++ controller/PartController.js
@@ -27,6 +27,71 @@ export const createPart = async (req, res, next) => {
   }
 };
 
+export const editPart = async (req, res, next) => {
+  try {
+    const {
+      _id,
+      partNumber,
+      name,
+      quantity,
+      price,
+      lowLimit,
+      description,
+    } = req.body;
+
+    const userData = await dataFunction(req, res, next);
+
+    const existingPart = await Part.findOne({
+      partNumber,
+      userId: userData.user._id,
+      _id: { $ne: _id },
+    });
+
+    if (existingPart) {
+      return res.status(409).json({
+        message: "Part number already exists.",
+      });
+    }
+
+    const updatedPart = await Part.findOneAndUpdate(
+      {
+        _id,
+        userId: userData.user._id,
+      },
+      {
+        partNumber,
+        name,
+        quantity,
+        price,
+        lowLimit,
+        description,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedPart) {
+      return res.status(404).json({
+        message: "Part not found.",
+      });
+    }
+
+    const parts = await Part.find({
+      userId: userData.user._id,
+    });
+
+    return res.status(200).json({
+      parts,
+      message: "Part updated.",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
 export const getData = async (req, res, next) => {
   try {
     const parts = await Part.find({ userId: req.user.id, deleted: false });
@@ -52,7 +117,7 @@ export const softDelete = async (req, res, next) => {
       });
     }
 
-    await newPart.save();
+    await deletePart.save();
 
     const parts = await Part.find({userId:  userData.user._id})
 
